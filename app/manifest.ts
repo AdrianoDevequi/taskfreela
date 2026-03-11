91e5bc9dda71db25888709aa13384cfa6e0f76a1
adrianodevequi <adrianodevequi@gmail.com>
2026-03-10 21:55:53 -0300

    fix: bump manifest cache versions

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,17 +11,17 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#0a0a0a',
         icons: [
             {
-                src: '/favicon.ico?v=2',
+                src: '/favicon.ico?v=3',
                 sizes: 'any',
                 type: 'image/x-icon',
             },
             {
-                src: '/icons/icon-192.png?v=2',
+                src: '/icons/icon-192.png?v=3',
                 sizes: '192x192',
                 type: 'image/png',
             },
             {
-                src: '/icons/icon-512.png?v=2',
+                src: '/icons/icon-512.png?v=3',
                 sizes: '512x512',
                 type: 'image/png',
             },
